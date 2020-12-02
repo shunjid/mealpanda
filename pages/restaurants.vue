aa2752837b7af1b2016b9ae4a5d6b40a5fb303d7
shunjid <shunjid.rahman@gmail.com>
2020-12-02 12:32:50 +0600

    created a filtering component

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,27 @@
 <template>
-  <main class="container restaurants">
-    <h1>Restaurants</h1>
+  <main class="container restaurant">
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+      <app-select @change="selectedRestaurant = $event" />
+      <pre>{{ $data }}</pre>
+    </div>
     <app-restaurant-info />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import AppSelect from "@/components/AppSelect.vue";
 
 export default {
   components: {
+    AppSelect,
     AppRestaurantInfo
+  },
+  data() {
+    return {
+      selectedRestaurant: ""
+    };
   }
 };
 </script>
