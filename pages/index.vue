9fc92832b0e4043a8d5e71f6158c54b80ca0de64
shunjid <shunjid.rahman@gmail.com>
2020-12-02 13:58:07 +0600

    filtered components using emit and datasource from parent

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
   <main class="container">
     <app-header />
-    <app-restaurant-info />
+    <app-restaurant-info :datasource="fooddata" />
   </main>
 </template>
 
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
     AppRestaurantInfo,
     AppHeader
+  },
+  computed: {
+    ...mapState(["fooddata"])
   }
 };
 </script>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -3,15 +3,15 @@
     <div class="restaurantheading">
       <h1>Restaurants</h1>
       <app-select @change="selectedRestaurant = $event" />
-      <pre>{{ $data }}</pre>
     </div>
-    <app-restaurant-info />
+    <app-restaurant-info :datasource="filteredRestaurants" />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
 import AppSelect from "@/components/AppSelect.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
@@ -22,6 +22,18 @@ export default {
     return {
       selectedRestaurant: ""
     };
+  },
+  computed: {
+    ...mapState(["fooddata"]),
+    filteredRestaurants() {
+      if (this.selectedRestaurant) {
+        return this.fooddata.filter(el => {
+          const name = el.name.toLowerCase();
+          return name.includes(this.selectedRestaurant);
+        });
+      }
+      return this.fooddata;
+    }
   }
 };
 </script>
